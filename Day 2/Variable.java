7263b677c48c618f1b20e0c2c5708233f2406d86
saloni2706 <saloniii2706@gmail.com>
2026-06-18 09:41:41 +0530

    Add variable declarations

---
+++ Day 2/Variable.java
@@ -0,0 +1,30 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        int x; //declare the variable
+        // int is called a "Data Type"
+
+        x=2; //put the value of 2 in the bucket named x. Also assigns the value to the variable
+
+        // x+2 = 4
+        // x*2 = 4
+        // x/2 = 1 
+
+        System.out.println(x+2); // +,-,*,/
+        System.out.println(x*2);
+        System.out.println(x/2);
+
+        x=6;
+        System.out.println(x+2); 
+        System.out.println(x*2);
+        System.out.println(x/2);
+
+        String name;
+        name="saloni";
+        System.out.println(name); //variable name
+        System.out.print("name"); //string name
+
+
+
+    }
+}
