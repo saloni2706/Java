43098fa5da90af63a13d9aca1b6fdce5a5909066
saloni2706 <saloniii2706@gmail.com>
2026-06-19 11:13:32 +0530

    completed variables and data type concepts in java.

---
+++ Day 3/VariablesContinued.java
@@ -0,0 +1,51 @@
+public class VariablesContinued {
+    public class InnerVariablesContinued {
+        public static void main(String[] args) {
+        
+    
+    
+    
+        int x = 1000;
+        System.out.println(x);
+
+        //range of numbers for int is 32 bits (32 bits= -2^31 to 2^31)
+        //to save a very very large no. we use data type "long"
+
+        long aa = 100000000000L;
+        System.out.println(aa);
+        //size of long in java is 64 bits (64 bits = -2^63 to 2^63 )
+        //Long also take up more memory as compared to an integer
+
+        short num=32000;
+        System.out.println(num);
+
+        //short data type has a size of 16 bits (16 bits = -2^15 to2^15)
+        //takes up less space
+
+        byte num1= 100;
+        System.out.println(num1);
+
+        //byte is another data type which is even smaller then short
+        //8 bits = -2^7 to 2^7
+
+        double num2 = 32.27483;
+        System.out.println(num2);
+
+        //data type which is used to save decimal is called double
+
+        boolean bool= true;
+        System.out.println(bool);
+        //boolean is a data type which save just two values either true or false
+
+        char character ='s';
+        System.out.println(character);
+
+        // char data type stores just a single character
+        //string is combination of characters
+
+
+        }
+
+    }
+}   
+
