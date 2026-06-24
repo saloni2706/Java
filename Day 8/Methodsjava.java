9d2aaa45e748034f00529e75c9bccb0ad35a30de
saloni2706 <saloniii2706@gmail.com>
2026-06-24 19:06:45 +0530

    Created First java method

---
+++ Day 8/Methodsjava.java
@@ -0,0 +1,25 @@
+public class Methodsjava {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+        //println is a method 
+        //"hello world" ia an argument
+
+        PrintSomething("saloni");  //calls the method. this was a method without an argument
+
+        
+
+        //calling a method : executing lines inside the method.
+
+        
+    }
+
+    public static void PrintSomething(String name){
+        System.out.println("My name is "+ name);
+
+    }
+        
+    }
+    
+
