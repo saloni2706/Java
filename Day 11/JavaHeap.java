972a217e7aeb82694a398e69dd7158ecaeaa0e81
saloni2706 <saloniii2706@gmail.com>
2026-07-22 08:24:05 +0530

    Fixed Java Heap Memory example by adding classes and correcting constructor syntax

--- Day 11/JavaHeap.java
+++ Day 11/JavaHeap.java
@@ -1,23 +1,57 @@
-public class JavaHeap {
-    public static void main(String[] args) {
-        double pi=3.14;
+class Processor {
+    String processorName;
 
-        Smartphone sm1;
-        sm1 = new Smartphone("Note 10","Samsung",50000,6,"Red");
-
-        //sm1 is not the actual object , it is a reference variable which is pointing to the object created in the heap memory.
-
-        sm1 = new Smartphone("A52","samsung",20000,6,"black");
-        //actual object ia created and stored in the heap memory
-        //pointers are stored in the stack memory.
-
-        int x= 10; //x has the actual value because int, double, boolean etc sre primitive data types
-
-        Processor proc1= new Processor(); //proc1 was a reference variable which points to a processor
-
-        sm1.proc= proc1;
-
+    Processor() {
+        processorName = "Snapdragon";
     }
-    
 }
 
+class Smartphone {
+    String modelName;
+    String brandName;
+    int price;
+    double screenSize;
+    String color;
+
+    Processor proc;
+
+    Smartphone(String modelName, String brandName, int price, double screenSize, String color) {
+        this.modelName = modelName;
+        this.brandName = brandName;
+        this.price = price;
+        this.screenSize = screenSize;
+        this.color = color;
+    }
+}
+
+public class JavaHeap {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14;
+
+        Smartphone sm1;
+
+        sm1 = new Smartphone("Note 10", "Samsung", 50000, 6.0, "Red");
+
+        // sm1 is a reference variable pointing to the object created in heap memory
+
+        sm1 = new Smartphone("A52", "Samsung", 20000, 6.0, "Black");
+
+        // Actual object is created in the heap memory
+        // Reference variables (pointers) are stored in the stack memory
+
+        int x = 10; // Primitive data types store actual values
+
+        Processor proc1 = new Processor();
+
+        sm1.proc = proc1;
+
+        System.out.println("Model : " + sm1.modelName);
+        System.out.println("Brand : " + sm1.brandName);
+        System.out.println("Price : " + sm1.price);
+        System.out.println("Screen Size : " + sm1.screenSize);
+        System.out.println("Color : " + sm1.color);
+        System.out.println("Processor : " + sm1.proc.processorName);
+    }
+}
